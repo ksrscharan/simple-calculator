7c74ba183d34a762207ac42957703a10885abcef
ramc9777 <ramc9777@gmail.com>
2021-12-25 17:37:08 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// const arr1 = [1,2,3,4,5]
+// function makeDouble(x){
+//     return 2*x
+// }
+// const arr2 = arr1.map(x=>makeDouble(x))
+
+let display = document.getElementById('display')
+let button = Array.from(document.getElementsByClassName("btn"))
+// btn
+function click(btn){
+    btn.addEventListener("click",(dispVal)=>{
+        switch(dispVal.target.innerText){
+            case 'C':
+                display.innerText = ""
+                break
+            case '←':
+                display.innerText = display.innerText.slice(0,-1)
+                break
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText)
+                    break
+                }catch{
+                    display.innerText = "Error"
+                    break
+                }
+            default:
+                if(display.innerText === ""){
+                    if(dispVal.target.innerText !='0'){
+                        display.innerText += dispVal.target.innerText
+                        break
+                    }
+                }else{
+                display.innerText += dispVal.target.innerText
+                break}
+        }
+    })
+}
+button.map(btn=>click(btn))
